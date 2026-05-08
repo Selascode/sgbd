08f5763b88737a4d9234971706db07152e96882d
Selascode <macleanskc@gmail.com>
2026-05-08 13:29:16 +0200

    new : Ajout des code des valeurs a tester avant de faire la suite

--- src/fr/insarouen/iti/prog/sgbd/modele/Type.java
+++ src/fr/insarouen/iti/prog/sgbd/modele/Type.java
@@ -1,6 +1,33 @@
 package fr.insarouen.iti.prog.sgbd.modele;
 
-public class Type {
+/**
+ * Énumération représentant les types de données supportés par le SGBD.
+ * <p>
+ * Chaque constante correspond à un type SQL et est utilisée pour typer les
+ * {@link Attribut attributs} d'une {@link Table table} et valider la cohérence
+ * des {@link Valeur valeurs} qui y sont insérées.
+ * </p>
+ *
+ * @see Attribut
+ * @see Valeur
+ */
+public enum Type {
 
-    
+    /**
+     * Type entier signé ({@code INTEGER} en SQL).
+     * Correspond aux valeurs portées par {@link ValeurInt}.
+     */
+    INT,
+
+    /**
+     * Type chaîne de caractères de longueur variable ({@code VARCHAR} en SQL).
+     * Correspond aux valeurs portées par {@link ValeurVarchar}.
+     */
+    VARCHAR,
+
+    /**
+     * Type entier auto-incrémenté ({@code SERIAL} en SQL / PostgreSQL).
+     * Utilisé typiquement pour les clés primaires générées automatiquement et gérer l'auto-incrémentation.
+     */
+    SERIAL
 }
--- src/fr/insarouen/iti/prog/sgbd/modele/Valeur.java
+++ src/fr/insarouen/iti/prog/sgbd/modele/Valeur.java
@@ -1,6 +1,22 @@
 package fr.insarouen.iti.prog.sgbd.modele;
 
-public class Valeur {
+import fr.insarouen.iti.prog.sgbd.modele.Type;
 
+public abstract class Valeur {
+    
+    private Type type; 
+
+    public Valeur(Type type){
+        this.type = type; 
+
+    }
+
+    public Type getType(){
+        return type;
+    }
+
+    public abstract Object getDonnee();
+    public abstract int compareA(Valeur autre);
+    public abstract boolean estCompatible(Type t);
     
 }
