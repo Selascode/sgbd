d7cbce12384a79bc4be58206326cf3808258c8f8
yelakoum <y_elakoum@insa-rouen.fr>
2026-05-08 18:30:17 +0200

    jai implementer la class Attribut avec les methodes

--- src/fr/insarouen/iti/prog/sgbd/modele/Attribut.java
+++ src/fr/insarouen/iti/prog/sgbd/modele/Attribut.java
@@ -1,6 +1,37 @@
 package fr.insarouen.iti.prog.sgbd.modele;
 
 public class Attribut {
+    private String nom;
+    private Type type;
+
+
+    /**
+     * Chaque attribut est caracterise par un nom et un type
+     * @param nom
+     * @param type
+     */
+
+    public Attribut(String nom,Type type){
+        this.nom=nom;
+        this.type=type;
+
+    }
+    /**
+     * 
+     * @return retourne le nom de l'attribut
+     */
+    public String getNom(){
+        return this.nom;
+    }
+
+    /**
+     * 
+     * @return retourne le type de l'attribut
+     */
+    public Type getType(){
+        return this.type;
+    }
+
 
     
 }
