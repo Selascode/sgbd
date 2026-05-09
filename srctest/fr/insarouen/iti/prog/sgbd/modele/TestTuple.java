dd1e52262bc70c2bc359c88d7f1d8c3fdf3b6e82
mguerrouache <marwan.guerrouache@insa-rouen.fr>
2026-05-09 14:33:24 +0200

    config des tests

--- srctest/fr/insarouen/iti/prog/sgbd/modele/TestTuple.java
+++ srctest/fr/insarouen/iti/prog/sgbd/modele/TestTuple.java
@@ -1,5 +1,12 @@
 package fr.insarouen.iti.prog.sgbd.modele;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestTuple {
 
     
