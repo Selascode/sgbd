bcad163045150484dd592127bf540eedd5e96f69
mohamedkamara <kamaracpge76@gmaiLcom>
2026-05-10 02:56:49 +0200

    [TEST] Ajout de la méthode main pour l'exécution du parseur SGBD et gestion des erreurs de syntaxe et lexicale

--- src/fr/insarouen/iti/prog/sgbd/execution/Interpreteur.java
+++ src/fr/insarouen/iti/prog/sgbd/execution/Interpreteur.java
@@ -1,6 +1,38 @@
 package fr.insarouen.iti.prog.sgbd.execution;
 
+import fr.insarouen.iti.prog.sgbd.parseur.SGBDParser;
+import fr.insarouen.iti.prog.sgbd.parseur.ParseException;
+import fr.insarouen.iti.prog.sgbd.parseur.TokenMgrError;
+import java.io.FileInputStream;
+
 public class Interpreteur {
 
-    
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println(" chemin vers un fichier .sql");
+            return;
+        }
+
+        try {
+            FileInputStream fis = new FileInputStream(args[0]);
+            SGBDParser parser = new SGBDParser(fis);
+            System.out.println("Analyse du fichier: " + args[0]);
+
+            // j'appelle la règle de base (Commande) en boucle jusqu'à la fin du fichier
+            while (true) {
+                try {
+                    parser.Commande();
+                    System.out.println("[OK] Commande syntaxiquement correcte !");
+                } catch (ParseException e) {
+                    System.out.println("[ERREUR] Erreur de syntaxe : " + e.getMessage());
+                    break;
+                } catch (TokenMgrError e) {
+                    System.out.println("[ERREUR Erreur lexicale : " + e.getMessage());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
