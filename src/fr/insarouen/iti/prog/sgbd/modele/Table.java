2be789724f91b257637a85c9be004573bf120617
Selascode <macleanskc@gmail.com>
2026-05-10 21:04:07 +0200

    [fix] Corrections des bugs et ajout de getAttributs

--- src/fr/insarouen/iti/prog/sgbd/modele/Table.java
+++ src/fr/insarouen/iti/prog/sgbd/modele/Table.java
@@ -1,6 +1,132 @@
 package fr.insarouen.iti.prog.sgbd.modele;
+import fr.insarouen.iti.prog.sgbd.modele.Attribut;
+import fr.insarouen.iti.prog.sgbd.modele.Tuple;
+import fr.insarouen.iti.prog.sgbd.exceptions.AttributInconnuException;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Représente une table au sein d'une base de données relationnelle.
+ * Une table possède un nom unique et contient un ensemble de tuples 
+ * respectant une structure d'attributs définie.
+ * 
+ * @author Elakoum
+ * @version 1.0
+ */
 
 public class Table {
+    /*Le nom de  la table dans une base de donne ce nom la est unique pour chaque table */
+
+    private String nom;
+    private List<Attribut> attributs;
+    private List<Tuple> tuples;
+   
+    /**
+     * Construction d'une table vide 
+     * @param nom tab a creer
+     */
+
+    public Table(String nom){
+        this.nom=nom;
+        this.attributs = new ArrayList<>();
+        this.tuples = new ArrayList<>();
+    }
+
+    /**
+     * Permet linsertion de tuple dans une table 
+     * @param t Tuple qui contient des valeur d'attribut
+     */
+    public void insererTuple(Tuple t){
+        this.tuples.add(t);
+    }
+
+
+
+    /***
+     * 
+     * @param tuplesASupprimer
+     * @return retourne le nb tuple qui sont supprimer 
+     */
+    public int supprimerTuples(List<Tuple> tuplesASupprimer) {
+        int tailleAvant = this.tuples.size();
+        this.tuples.removeAll(tuplesASupprimer);
+        int tailleApres = this.tuples.size();
+        
+        return tailleAvant - tailleApres; // La différence nous donne le nombre de lignes supprimées
+    }
+
+    /**
+     * 
+     * @return retourne la liste des tuples de la tables
+     * 
+     */
+    public List<Tuple> getTuples(){
+        return Collections.unmodifiableList(this.tuples); 
+    }
+
+    /**
+     * @param nom
+     * @return l 'attribut dont il ya le nom nom
+     */
+    public Attribut getAttribut(String nom) throws AttributInconnuException{//la je pene il sera mieux si en ceera uen exception pour dire si l'attrinbut exuste ou pas
+       
+            for( Attribut att :this.attributs){
+                if(nom.equals(att.getNom()) ){
+                    return att;
+                }
+            }
+            throw new AttributInconnuException(nom);//ici en doit enlever 
+    }
+     /**
+     * @param 
+     * @return l 'attribut dont il ya le nom nom
+     */
+
+    public List<Attribut> getAttributs(){
+        return Collections.unmodifiableCollection(this.attributs)
+    }
+    /***
+     * 
+     * @param nom
+     * @return un boolean en indiquant si l attribut avec ce nom la existe ou pas 
+     */
+    public boolean attributExiste(String nom){
+        for (Attribut att : this.attributs){
+            if (nom.equals(att.getNom())){
+                return true; 
+            }
+        }
+        return false // dapres la java doc cette methode retourn true si existe false si non 
+    }
+
+    /***
+     * Pour obtenir l'indice dun attribut pour gerer apres le auto incremente  
+     * @param nom
+     * @return lindice de l'attribut possedent le nom nom
+    */
+    public int indexAttribut(String nom){//la encore il faut ajouter lexceprion ou bien throws car en utilise un methode qui est get attribut qui peut lever une exceprion 
+        Attribut att=this.getAttribut(nom);
+        int index= this.attributs.indexOf(att);//dapres la java doc (Returns the index of the first occurrence of the specified element in this list, or -1 if this list does not contain the elemen)
+        return index;
+    }
+
+    /**
+     * @return le nom de la table
+     */
+    public String getNom() {
+        return this.nom;
+    }
+
+    /**
+     * Permet d'ajouter un attribut (unbe colonne) au schma de la table
+     * @param a L'attribut à ajouter
+     */
+    public void addAttribut(Attribut a) {
+        this.attributs.add(a);
+    }
 
         
 }
--- src/fr/insarouen/iti/prog/sgbd/modele/Tuple.java
+++ src/fr/insarouen/iti/prog/sgbd/modele/Tuple.java
@@ -1,6 +1,49 @@
 package fr.insarouen.iti.prog.sgbd.modele;
 
-public class Tuple {
+import fr.insarouen.iti.prog.sgbd.modele.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+
+/**
+ * Représente un tuple (une ligne de données) dans une table.
+ * Un tuple est une collection ordonnée de valeurs correspondant aux attributs de la table.
+ * * @author Elakoum
+ * @version 1.0
+ */
+
+
+public class Tuple {
+    private List<Valeur> values;
+    public Tuple(List<Valeur> values){
+        this.values=values;//new ArrayList<>(values);
+
+    }
+
+    /**
+     * 
+     * @param index indice de l'attribut  dans la table 
+     * @return la valeur associer a cette attribut
+     */
+    public Valeur getValeur(int index_attribut){
+
+        return this.values.get(index_attribut);//selon la java doc (Returns the element at the specified position in this list.)
+
+    }
+    /**
+     * 
+     * @return retourne les valeurs dun tuple
+     */
+    public List<Valeur> getValeurs(){
+        return Collections.unmodifiableList(this.values);// Comme dans le projet aventure pour avoir une copie pas une liste avec la même adresse
+    }
+
+    /**
+     * @return retourn la taille dun tuple
+     */
+    public int taille(){
+        return this.values.size();
+    }
     
 }
