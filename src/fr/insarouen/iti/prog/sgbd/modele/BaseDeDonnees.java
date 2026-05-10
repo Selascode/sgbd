ab9f61756e24abf10dbd6261a3cd8f21ac6cc94f
mohamedkamara <kamaracpge76@gmaiLcom>
2026-05-10 18:45:55 +0200

    Ajout des attributs nom, tables et compteurSerial à la classe BaseDeDonnees

--- src/fr/insarouen/iti/prog/sgbd/modele/BaseDeDonnees.java
+++ src/fr/insarouen/iti/prog/sgbd/modele/BaseDeDonnees.java
@@ -1,6 +1,26 @@
 package fr.insarouen.iti.prog.sgbd.modele;
 
-public class BaseDeDonnees {
+import fr.insarouen.iti.prog.sgbd.exceptions.ErreurTableExistante;
+import fr.insarouen.iti.prog.sgbd.exceptions.ErreurTableInconnue;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Représente la base de données principale.
+ * Gère l'ensemble des tables et le compteur global pour les types SERIAL
+ */
+public class BaseDeDonnees {
+    private String nom;
+    private Map<String, Tables> tables;
+    private int compteurSerial;
+
+    public BaseDeDonnees(String nom){
+        this.nom = nom;
+        this.tables = new HashMap<>();
+        this.compteurSerial = 0;
+    }
         
 }
