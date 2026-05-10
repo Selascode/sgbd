55b98e4a5acc2aa605ba4a249cc28e0deb834455
mohamedkamara <kamaracpge76@gmaiLcom>
2026-05-10 18:30:05 +0200

    Ajout d'un test unitaire pour la méthode addAttribut dans la classe TestTable qui est incomplet

--- srctest/fr/insarouen/iti/prog/sgbd/modele/TestTable.java
+++ srctest/fr/insarouen/iti/prog/sgbd/modele/TestTable.java
@@ -1,6 +1,18 @@
 package fr.insarouen.iti.prog.sgbd.modele;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestTable {
 
-    
+    @Test
+    public void testAddAttribut() {
+        Table t = new Table("Hero");
+        Attribut a = new Attribut("id", Type.SERIAL);
+        t.addAttribut(a);
+
+        assertTrue(t.attributExiste("id"));
+        assertEquals(a, t.getAttribut("id"));
+        assertEquals(0, t.indexAttribut("id"));
+    }
 }
