6bfcce0eefa38b161fb54bf420ad601f62f6861b
Selasi Maclean <smaclean@insa-rouen.fr>
2026-05-11 08:44:37 +0200

    [test] Ajout des tests de Attribut

--- srctest/fr/insarouen/iti/prog/sgbd/modele/AllTestsModele.java
+++ srctest/fr/insarouen/iti/prog/sgbd/modele/AllTestsModele.java
@@ -4,11 +4,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import fr.insarouen.iti.prog.sgbd.modele.TestValeur;
 import fr.insarouen.iti.prog.sgbd.modele.TestTuple;
+import fr.insarouen.iti.prog.sgbd.modele.TestAttribut;
 
 @RunWith(Suite.class)
 @SuiteClasses({
     TestValeur.class,
-    TestTuple.class
+    TestTuple.class,
+    TestAttribut.class,
 })
 
 public class AllTestsModele {}
