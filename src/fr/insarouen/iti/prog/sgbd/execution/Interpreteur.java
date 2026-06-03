6c7f65baa95a8c81d95722a99a837fb27213ef41
Selasi Maclean <smaclean@insa-rouen.fr>
2026-06-03 14:41:20 +0200

    [new] Ajout de source <Fichiers>

--- src/fr/insarouen/iti/prog/sgbd/execution/Interpreteur.java
+++ src/fr/insarouen/iti/prog/sgbd/execution/Interpreteur.java
@@ -157,7 +157,8 @@ public class Interpreteur {
                 commande += ligne;
 
                 String commandeLower = commande.trim().toLowerCase();
-
+                String commandeTrim  = commande.trim();
+                
                 if (commandeLower.equals("menu") || commandeLower.equals("menu;")) {
                     System.out.println("Retour au menu principal...\n");
                     return;
@@ -168,6 +169,23 @@ public class Interpreteur {
                     System.out.println("Au revoir ;)");
                     System.exit(0);
                 }
+
+                if(commandeLower.startsWith("source ")){
+                    String cheminFichier = commandeTrim.substring(7).trim();
+                    cheminFichier = cheminFichier.substring(0, cheminFichier.length() - 1).trim();
+                  
+
+                    if (cheminFichier.isEmpty()){
+                        System.out.println("[ERREUR] Syntaxe : source <chemin_vers_fichier.sql>"); 
+                    } else {
+                        System.out.println("Execution du fichier : " + cheminFichier);
+                        interpreteurFichier(cheminFichier, db, gestionnaire, chemin);
+                    }
+
+                    commande = "";
+                    System.out.print("sgbd> ");
+                }
+
 
                 if (commande.trim().endsWith(";")) {
                     commande = commande.trim();
@@ -192,6 +210,7 @@ public class Interpreteur {
 
                     commande = "";
                     System.out.print("sgbd> ");
+                    continue; 
                 }
             }
         } catch (java.util.NoSuchElementException e) {
